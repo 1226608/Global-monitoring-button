8052c8c63f0ac38eefc316dbebb5db319d91d1a0
yeyinyv <46079089+1226608@users.noreply.github.com>
2023-09-24 22:00:23 +0800

    Update and rename 全局监测按键.py to 钩子全局监测按键.py

--- 全局监测按键.py
+++
@@ -1,6 +0,0 @@
-import ctypes
-from pynput import keyboard
-def 钩子回调(编码, w参数, l参数):
-    ctypes.WinDLL('user32').CallNextHookEx(ctypes.WinDLL('user32').SetWindowsHookExA(13, ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int), 0, 0)(钩子回调), 编码, w参数, l参数)
-keyboard.Listener(on_press=lambda key: (print(key), key)).start()
-ctypes.windll.user32.GetMessageA(ctypes.byref(ctypes.wintypes.MSG()), 0, 0, 0)
